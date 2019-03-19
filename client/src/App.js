c98fefb95da40c7f256415922bdbe654ab7314c5
Daniel Ruales <danielmruales@gmail.com>
2019-03-19 15:01:47 -0600

    file setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-
+import {Switch, Route} from 'react-router-dom'
+import Navbar from './Navbar/Navbar'
+import Home from './Home/Home'
+import Menu from './Menu/Menu'
+import Catering from './Catering/Catering'
+import About from './About/About'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet"></link>
-          <h1>Eva's Bakery </h1>
+      <div>
+          <Navbar/>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route path='/menu' component={Menu}/>
+              <Route path='/catering' component={Catering}/>
+              <Route path='/about' component={About}/>
+            </Switch>
+          <Footer/>
       </div>
     );
   }
