83d80c0429cc3cf5556eb4357b8392ebe427e718
Daniel Ruales <danielmruales@gmail.com>
2019-03-19 14:16:33 -0600

    setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet"></link>
+          <h1>Eva's Bakery </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
