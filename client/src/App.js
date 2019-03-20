8a706cc1a6a4444e7e2386a0297980590dee9dc8
Daniel Ruales <danielmruales@gmail.com>
2019-03-20 11:14:58 -0600

    navbar implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import {Switch, Route} from 'react-router-dom'
-// import Navbar from './Navbar/Navbar'
+import Navbar from './Navbar/Navbar'
 // import Home from './Home/Home'
 // import Menu from './Menu/Menu'
 // import Catering from './Catering/Catering'
@@ -11,14 +11,14 @@ class App extends Component {
   render() {
     return (
       <div>
-          {/* <Navbar/>
-            <Switch>
+          <Navbar/>
+            {/* <Switch>
               <Route exact path='/' component={Home}/>
               <Route path='/menu' component={Menu}/>
               <Route path='/catering' component={Catering}/>
               <Route path='/about' component={About}/>
-            </Switch>
-          <Footer/> */}
+            </Switch> */}
+          {/* <Footer/> */}
       </div>
     );
   }
