392ecfbb9ba8777eec1c6a8299df2d31c874e919
christian makoma <nkndchris@gmail.com>
2019-03-20 10:30:00 -0600

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
-import Home from './Home/Home'
-import Menu from './Menu/Menu'
-import Catering from './Catering/Catering'
-import About from './About/About'
+// import Home from './Home/Home'
+// import Menu from './Menu/Menu'
+// import Catering from './Catering/Catering'
+// import About from './About/About'
 import './App.css';
 
 class App extends Component {
@@ -12,13 +12,13 @@ class App extends Component {
     return (
       <div>
           <Navbar/>
-            <Switch>
+            {/* <Switch>
               <Route exact path='/' component={Home}/>
               <Route path='/menu' component={Menu}/>
               <Route path='/catering' component={Catering}/>
               <Route path='/about' component={About}/>
             </Switch>
-          <Footer/>
+          <Footer/> */}
       </div>
     );
   }
