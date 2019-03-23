4a16c711d900ff047bfeb42d3caf298613b0eff4
Kyle Allred <g.kyleallred@yahoo.com>
2019-03-22 18:07:07 -0600

    hmm

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
 import Home from './Home/Home'
+import Admin from './Admin/Admin'
 // import Menu from './Menu/Menu'
 import Catering from './Catering/Catering'
 // import About from './About/About'
@@ -10,9 +12,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <Home/>
-        <Catering/>
+        <Switch>
+          <Route exact path='/admin' component={Admin}/>
+          </Switch>
+          <Navbar/>
+          <Home/>
+          <Catering/>
       </div>
     );
   }
