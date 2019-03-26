de6459dff78ae7dc0afa7ef00ad43e58ce702b1e
christian makoma <nkndchris@gmail.com>
2019-03-26 00:30:38 -0600

    the admin side can now post and delete

--- client/src/Admin/EditMenus.js
+++ client/src/Admin/EditMenus.js
@@ -2,63 +2,29 @@ import React, { Component } from 'react';
 import Navbar from './Navbar'
 import axios from 'axios'
 import {withGlobalData} from '../GlobalData'
+import BreakfastEdit from "./BreakfastEdit.js"
+import LunchEdit from "./LunchEdit.js"
+import SaladEdit from "./SaladEdit.js"
 
 class EditMenus extends Component {
 
     componentDidMount(){
-        this.props.getLunchMenu()
-        this.props.getBreakfastMenu()
+  this.props.getSaladMenu()
     }
 
-    deleteLunchMenuItem = (e) =>{
-        axios.delete(`/lunchmenu/${e.target.name}`)
+    deleteSaladMenuItem = (e) =>{
+        axios.delete(`/saladmenu/${e.target.name}`)
     }
+
+    
 
     render() {
-        const mappedBreakfastMenu = this.props.breakfastMenuItems.map((breakfastMenuItem, i) => {
-            return (
-                <div className='adminItems'>
-                    <h1>{breakfastMenuItem.title}</h1>
-                    <h2>{breakfastMenuItem.description}</h2>
-                </div>
-            )
-        }) 
-
-        const mappedLunchMenu = this.props.lunchMenuItems.map((lunchMenuItem, i) => {
-            return (
-                <div className='adminItems'>
-                    <h1>{lunchMenuItem.title}</h1>
-                    <h2>{lunchMenuItem.description}</h2>
-                    <form className='adminMenuBtn' action="" name={lunchMenuItem._id} onSubmit={this.deleteLunchMenuItem}>
-                        <button>Delete</button>
-                    </form>
-                        <button>Edit</button>
-                </div>
-            )
-        }) 
-
-
-
-
-
-
-        return (
+    return (
             <div>
                 <Navbar/>
-                <h1 className='titleMenu'>Breakfast Menu</h1>
-                <div className='adminMenu'>
-                    {mappedBreakfastMenu}
-                </div>
-                <h1 className='titleMenu'>Lunch Menu</h1>
-                <div className='adminMenu'>
-                    {mappedLunchMenu}
-                </div>
-                <form className='adminForm'>
-                    <input type='text' placeholder='Item Title'></input>
-                    <input type='text'></input>
-                    <input type='number' min='0' required></input>
-                    <button>Add Item</button>
-                </form>
+                <BreakfastEdit/>
+                <LunchEdit />
+                <SaladEdit />
             </div>
         );
     }
