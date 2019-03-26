539ed8107cc3eded4deb4cbd4b25267360a63b51
Kyle Allred <g.kyleallred@yahoo.com>
2019-03-26 10:23:07 -0600

    simple addition

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -5,11 +5,11 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='socialContainer'>
-            <SocialIcon url='https://github.com/danielmruales/evas-bakery' className='socialIcon'/>
-            <SocialIcon url='https://www.twitter.com/EvasBakery' className='socialIcon'/>
-            <SocialIcon url='https://www.facebook.com/EvasBakerySLC' className='socialIcon'/>
-            <SocialIcon url='https://www.instagram.com/EvasBakery' className='socialIcon'/>
-            <SocialIcon url='https://www.yelp.com/biz/evas-bakery-salt-lake-city' className='socialIcon'/>
+            <SocialIcon url='https://github.com/danielmruales/evas-bakery' className='socialIcon' target="_blank norefer noopener"/>
+            <SocialIcon url='https://www.twitter.com/EvasBakery' className='socialIcon' target="_blank norefer noopener"/>
+            <SocialIcon url='https://www.facebook.com/EvasBakerySLC' className='socialIcon' target="_blank norefer noopener"/>
+            <SocialIcon url='https://www.instagram.com/EvasBakery' className='socialIcon' target="_blank norefer noopener"/>
+            <SocialIcon url='https://www.yelp.com/biz/evas-bakery-salt-lake-city' className='socialIcon' target="_blank norefer noopener"/>
         </div>
     );
 };
