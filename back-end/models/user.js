f3b8fe6fcbcc6c9675f682ffcb5746c1a9c1a08c
Daniel Ruales <danielmruales@gmail.com>
2019-03-27 20:04:43 -0600

    fixed a portion of user auth

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        required: TextTrackCue, 
+        required: true 
     },
     isAdmin: {
          type: Boolean,
